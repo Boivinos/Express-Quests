5f2c388ec9cc05335239b8c3ed1c2dabae2683e6
Boivinos <boivinantonin@gmail.com>
2023-05-09 12:00:09 +0200

    express 4

--- app.js
+++ app.js
@@ -18,12 +18,13 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
-
+app.put("/api/movies/:id", movieHandlers.putMovieById);
 
 const userHandlers = require("./userHandler");
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", userHandlers.postUsers);
+app.put("/api/users/:id", userHandlers.putUserById);
 
 app.listen(port, (err) => {
   if (err) {
