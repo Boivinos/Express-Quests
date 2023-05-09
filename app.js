ea6e954b08be479695ce67dd231be37849a4e8ed
Boivinos <boivinantonin@gmail.com>
2023-05-09 16:21:07 +0200

    express 4 bis data validation with joi

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-
 const app = express();
-
 app.use(express.json());
+const { validateMovie, validateUser } = require("./validators.js");
+  
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -17,15 +17,15 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.putMovieById);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovie,movieHandlers.putMovieById);
 app.delete("/api/movies/:id", movieHandlers.deleteMovieById);
 
 const userHandlers = require("./userHandler");
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUsers);
-app.put("/api/users/:id", userHandlers.putUserById);
+app.post("/api/users", validateUser, userHandlers.postUsers);
+app.put("/api/users/:id", validateUser, userHandlers.putUserById);
 app.delete("/api/users/:id", userHandlers.deleteUserById);
 
 app.listen(port, (err) => {
